141f2088956e8e32e8ced98e523a58abbad181e4
EC2 Default User <ec2-user@ip-172-31-80-47.ec2.internal>
2022-06-17 02:59:25 +0000

    first step fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  root 'public/homes#show'
   devise_for :admins
   devise_for :customers
 
@@ -9,13 +10,19 @@ Rails.application.routes.draw do
     resources :orders, only: [:show, :update]
     resources :order_details, only: [:update]
     resources :genres, only: [:index, :create, :edit, :update]
+    resources :customers, only: [:index, :show, :edit, :update]
   end
   namespace :public do
-    root "homes#show"
-    get "home/about"=>"homes#about"
+    get "/about"=>"homes#about"
     resources :items, only: [:index, :show]
-    resources :orders, only: [:new, :success, :index, :show, :confirm, :create]
-    resources :cart_items, only: [:index, :create, :update, :destroy, :destroy_all]
+    resources :orders, only: [:new, :index, :show, :create]
+    get "/orders/success"=>"orders#success"
+    post "/orders/confirm"=>"orders#confirm"
+    resources :customers, only:[:show ,:edit,:update]
+    patch "/customers/withdraw" =>"customers#withdraw"
+    get "/customers/result" => "customers#result"
+    resources :cart_items, only: [:index, :create, :update, :destroy]
+    delete "/cart_items/destroy_all" => "cart_items#destroy_all"
     resources :shipping_adresses, only: [:index, :edit, :create, :update, :destroy]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html

2a7ce0222233f0a7566a9f4385ac40ea16112cef
na08skt <sakata.na08@gmail.com>
2022-06-18 14:33:39 +0000

    preFinishCommit

---
+++ app/controllers/admin/items_controller.rb
@@ -0,0 +1,29 @@
+class Admin::ItemsController < ApplicationController
+  def index
+    @items = Item.all
+  end
+
+  def new
+    @item = Item.new
+    @genre = Genre.all
+  end
+
+  def create
+    @item = Item.new(item_params)
+    @item.save
+    redirect_to admin_item_path(@item)
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:image, :name, :introduction, :tax_off_price, :selling_status)
+  end
+
+
+end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,4 @@
 class Item < ApplicationRecord
- belongs_to :genre
+ belongs_to :genre, optional: true
+ has_one_attached :image
 end
